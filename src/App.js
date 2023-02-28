40b17d939f815789507a0e056555016358bd81e3
Anders Bogan <adbogan@cox.net>
2023-02-27 21:33:39 -0800

    package changes to work with webpack 5, new api key, face recognition calculations and display

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Navigation from './components/Navigation/Navigation'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import Logo from './components/Logo/Logo'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import Rank from './components/Rank/Rank'
@@ -7,32 +8,55 @@ import ParticlesBg from 'particles-bg'
 import React from 'react';
 import Clarifai from 'clarifai';
 
-// const app = new Clarifai.App({
-//   apiKey: 'f09f9c7b234047cf908aec29aee5e449'
-// });
+const app = new Clarifai.App({
+  apiKey: '41fa57bb4c634a15ad2491e03c93872b'
+});
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       input: '',
+      imageURL: '',
+      box: {}
     }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    console.log(box);
+    this.setState({box: box});
   }
 
   onInputChange = (event) => {
     console.log(event.target.value);
+    this.setState({input: event.target.value})
   }
 
   onButtonSubmit = () => {
     console.log('click');
-    // app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-    //   function(response) {
+    this.setState({imageURL: this.state.input})
+    app.models.predict({
+      id: 'face-detection',
+      name: 'face-detection',
+      version: '6dc7e46bc9124c5c8824be4822abe105',
+      type: 'visual-detector',
+      }, this.state.input)
+    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err));
 
-    //   },
-    //   function(err) {
-
-    //   }
-    // );
   }
 
   render() {
@@ -48,7 +72,7 @@ class App extends React.Component {
         <ImageLinkForm 
           onInputChange={this.onInputChange} 
           onButtonSubmit={this.onButtonSubmit}/>
-        {/*<FaceRecognition /> */ }
+        {<FaceRecognition box={this.state.box} imageURL={this.state.imageURL} />}
       </div>
     );
   }
