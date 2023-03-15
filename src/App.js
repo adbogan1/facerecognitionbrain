8907fcc1ea7de4ac1f92376e7ce979755142ea78
Anders Bogan <adbogan@cox.net>
2023-03-14 20:51:36 -0700

    Revert "Revert "moving API connection from localhost to online""
    This reverts commit f1270df958cbda1b6bd6a19ede7cacd948fd2ce4.

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ class App extends React.Component {
 
   onButtonSubmit = () => {
     this.setState({imageURL: this.state.input})
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://facerecognitionbrain-api-rk5b.onrender.com/imageurl', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -72,7 +72,7 @@ class App extends React.Component {
         .then(response => response.json())
         .then(response => {
           if (response) {
-            fetch('http://localhost:3000/image', {
+            fetch('https://facerecognitionbrain-api-rk5b.onrender.com/image', {
               method: 'put',
               headers: {'Content-Type': 'application/json'},
               body: JSON.stringify({
